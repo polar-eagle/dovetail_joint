5c895b257b44cbb814414851c1d9445ed7cc7460
Zhennan.TIAN <826531725@qq.com>
2025-01-14 20:31:00 +0800

    fixed invalid radius

--- data_generate.py
+++ data_generate.py
@@ -12,10 +12,15 @@ for num in range(3, 6, 1):
         width_up = width_base * (10.0 + ra1) / 10.0
         for ra2 in range(-5, 8, 4):
             height = width_base * (10.0 + ra2) / 10.0
-            radius_base = height / (2.0*math.cos(math.atan2(height, (width_up - width_root)/2.0)) + 2.0) * 0.95
+            radius_base1 = height / (2.0*math.cos(math.atan2(height, (width_up - width_root)/2.0)) + 2.0) * 0.95
+            radius_base2 = (width_up - width_root) / 4.0 * math.tan(math.atan2(height, (width_up - width_root)/2.0) / 2.0) * 0.95
+            radius_base2 = min(radius_base1, radius_base2)
             for ra3 in range(0, 11, 5):
-                radius = radius_base * ra3 / 10.0
                 for stage in range(1, 3, 1):
                     if height*stage > 4.9:
                         continue
+                    if stage == 1:
+                        radius = radius_base1 * ra3 / 10.0
+                    else:
+                        radius = radius_base2 * ra3 / 10.0
                     file.write(f"{num} {height:.2f} {width_up:.2f} {width_root:.2f} {stage} {radius:.2f} {num}_{stage}_{ra1}_{ra2}_{ra3}\n")
